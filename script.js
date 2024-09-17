9bb3f86accb2cef0a32b3fccf179291bad1e7000
Julie <julieglion@gmail.com>
2024-09-17 13:29:13 +0200

    add optimized solution

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const answerText = document.querySelector(".answer");
 const button = document.querySelector(".button");
-const input = document.getElementById("guess");
 
 let number = Math.round(Math.random() * 100);
 console.log(number);
@@ -8,12 +7,13 @@ console.log(number);
 button.addEventListener("click", checkNumber);
 
 function checkNumber() {
-  console.log(input.value);
+  const input = document.getElementById("guess").valueAsNumber;
+  console.log(input);
 
-  if (number === parseInt(input.value)) {
+  if (number === input) {
     answerText.classList.add("animation");
     answerText.innerText = "You guessed the number";
-  } else if (number > parseInt(input.value)) {
+  } else if (number > input) {
     answerText.innerText = "Your number is to low";
   } else {
     answerText.innerText = "Your number is to high";
